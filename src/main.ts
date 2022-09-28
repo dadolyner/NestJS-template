16445a9e28ca2a82591d694c9fbfb981dbfc5b8d
dadolyner <skulj.david@gmail.com>
2022-09-28 15:52:45 +0200

    register cookies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { AppModule } from './modules/app.module'
 import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+import fastifyCookie from '@fastify/cookie'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
-import { AppModule } from './modules/app.module'
 
+// Env Config
 import { config } from 'dotenv'
 import { resolve } from 'path'
 config({ path: resolve(__dirname, `../env/.env.${process.env.ENVIROMENT}`) })
@@ -15,10 +17,13 @@ const Application = async () => {
         logger.log(`Starting the application in ${process.env.ENVIROMENT} mode ...`)
 
         const Application = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), { cors: true })
+        await Application.register(fastifyCookie, { secret: process.env.COOKIE_SECRET });
+        Application.useGlobalPipes(new ValidationPipe())
+        
         const swaggerConfig = new DocumentBuilder().setTitle('Application').setDescription('API Documentation for Application.').setVersion('1.0').build()
         const swaggerDocument = SwaggerModule.createDocument(Application, swaggerConfig)
         SwaggerModule.setup('documentation', Application, swaggerDocument)
-        Application.useGlobalPipes(new ValidationPipe())
+        
         await Application.listen(process.env.PORT, '0.0.0.0')
         
         logger.log(`Application is running on ${process.env.SERVER_IP} in ${process.env.ENVIROMENT} mode.`)
