9ebbf8500711499cf8c68028b19ea1f2fb9dc1c5
dadolyner <skulj.david@gmail.com>
2022-09-29 14:55:10 +0200

    swagger documentation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -6,24 +6,32 @@ import { AuthLoginDto, AuthRegisterDto } from './dto/auth.dto'
 import { FastifyReply } from 'fastify'
 import { Cookie } from './decorator/cookie.decorator'
 import { AuthGuard } from './jwt/auth.guard'
+import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('Authentification')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
     
     // Register new user
+    @ApiResponse({ status: 201, description: 'Create new user' })
+    @ApiBody({ type: AuthRegisterDto })
     @Post('register')
     async register(@Body() registerDto: AuthRegisterDto): Promise<void> {
         await this.authService.register(registerDto)
     }
     
     // Login existing user, generate tokens and set cookies (user, access_token, refresh_token)
+    @ApiResponse({ status: 200, description: 'Login existing user' })
+    @ApiBody({ type: AuthLoginDto })
     @Post('login')
     async login(@Body() loginDto: AuthLoginDto, @Res() response: FastifyReply): Promise<void> {
         await this.authService.login(loginDto, response)
     }
     
     // Refresh access token with a valid refresh token
+    @ApiResponse({ status: 200, description: 'Refresh users access token' })
+    @ApiBearerAuth()
     @UseGuards(AuthGuard)
     @Post('refresh')
     async refreshToken(@Cookie('user') user: string, @Cookie('refresh_token') refreshToken: string, @Res() response: FastifyReply): Promise<void> {
@@ -31,6 +39,8 @@ export class AuthController {
     }
     
     // Logout user, remove refresh token and clear cookies
+    @ApiResponse({ status: 200, description: 'Logout user and clear its cookies' })
+    @ApiBearerAuth()
     @UseGuards(AuthGuard)
     @Post('logout')
     async logout(@Cookie('user') user: string, @Res() response: FastifyReply): Promise<void> {
--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator'
 export class AuthRegisterDto {
+    @ApiProperty({ type: String, description: 'User first name', example: 'Janez' })
     @IsString({ message: 'First name must be a string' })
     @IsNotEmpty({ message: 'First name is required' })
     first_name: string
 
+    @ApiProperty({ type: String, description: 'User last name', example: 'Novak' })
     @IsString({ message: 'Last name must be a string' })
     @IsNotEmpty({ message: 'Last name is required' })
     last_name: string
 
+    @ApiProperty({ type: String, description: 'User email', example: 'janez.novak@gmail.com' })
     @IsString({ message: 'Email must be a string' })
     @IsEmail({ message: 'This is not an email' })
     @IsNotEmpty({ message: 'Email is required' })
     email: string
 
+    @ApiProperty({ type: String, description: 'User password', example: 'janeznovak123' })
     @IsString({ message: 'Password must be a string' })
     @MinLength(8, { message: 'Password is too short' })
     @MaxLength(100, { message: 'Password is too long' })
@@ -20,11 +25,13 @@ export class AuthRegisterDto {
 }
 
 export class AuthLoginDto {
+    @ApiProperty({ type: String, description: 'User email', example: 'janez.novak@gmail.com' })
     @IsString({ message: 'Email must be a string' })
     @IsEmail({ message: 'This is not an email' })
     @IsNotEmpty({ message: 'Email is required' })
     email: string
 
+    @ApiProperty({ type: String, description: 'User password', example: 'janeznovak123' })
     @IsString({ message: 'Password must be a string' })
     @MinLength(8, { message: 'Password is too short' })
     @MaxLength(100, { message: 'Password is too long' })
