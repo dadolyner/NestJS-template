cf513fa893d29a2b934518f456fe203228cbff26
dadolyner <skulj.david@gmail.com>
2023-02-02 15:44:10 +0100

    guard cleanup

--- src/modules/auth/guard/auth.guard.ts
+++ src/modules/auth/guard/auth.guard.ts
@@ -6,59 +6,43 @@ import DadoEx from 'src/helpers/exceptions'
 // Acces Token Guard
 @Injectable()
 export class AccessGuard extends AuthGuard('jwt') {
-    private dadoEx = new DadoEx(AccessGuard.name)
-
     canActivate(context: ExecutionContext): any {
         const request = context.switchToHttp().getRequest()
         const response = context.switchToHttp().getResponse()
         request.response = response
-
-        try { return super.canActivate(context) }
-        catch (error) { this.dadoEx.throw({ status: 401, message: `Invalid access token.`, response }) }
+        return super.canActivate(context)
     }
 }
 
 // Refresh Token Guard
 @Injectable()
 export class RefreshGuard extends AuthGuard('jwt-refresh') {
-    private dadoEx = new DadoEx(RefreshGuard.name)
-
     canActivate(context: ExecutionContext): any {
         const request = context.switchToHttp().getRequest()
         const response = context.switchToHttp().getResponse()
         request.response = response
-
-        try { return super.canActivate(context) }
-        catch (error) { this.dadoEx.throw({ status: 401, message: `Invalid refresh token.`, response }) }
+        return super.canActivate(context)
     }
 }
 
 // Password reset Token Guard
 @Injectable()
 export class PasswordGuard extends AuthGuard('jwt-password') {
-    private dadoEx = new DadoEx(PasswordGuard.name)
-
     canActivate(context: ExecutionContext): any {
         const request = context.switchToHttp().getRequest()
         const response = context.switchToHttp().getResponse()
         request.response = response
-
-        try { return super.canActivate(context) }
-        catch (error) { this.dadoEx.throw({ status: 401, message: `Invalid password token.`, response }) }
+        return super.canActivate(context)
     }
 }
 
 // Email verification Token Guard
 @Injectable()
 export class EmailGuard extends AuthGuard('jwt-email') {
-    private dadoEx = new DadoEx(EmailGuard.name)
-
     canActivate(context: ExecutionContext): any {
         const request = context.switchToHttp().getRequest()
         const response = context.switchToHttp().getResponse()
         request.response = response
-
-        try { return super.canActivate(context) }
-        catch (error) { this.dadoEx.throw({ status: 401, message: `Invalid email token.`, response }) }
+        return super.canActivate(context)
     }
 }
